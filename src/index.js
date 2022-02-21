ca7e1895a814f67dd21c7aefc9cf1f88e6fcbb73
Lubov <lubaparfyonova@gmail.com>
2022-02-21 19:37:54 +0200

    Add react render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-//const _ = require('lodash');
-const Component = require('./Component');
-require('./css/style.css');
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './css/style.css';
 
-//_.random();
-const container = document.getElementById('root');
-container.append('Hello');
+
+// JSX tag:
+// <type props="value">children</type>
+
+class HelloWorld extends Component {
+
+    render () {
+//       return React.createElement('p', {title: 'hello'}, 'Hello from react');
+        return <p title="hello" className="paragraph">Hello from <span> react </span> </p>
+
+    }
+}
+
+const compHello = React.createElement(HelloWorld);
+
+const root = document.getElementById('root');
+
+ReactDOM.render(compHello, root);
